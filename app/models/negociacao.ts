f7bf66d451656e67cc13e725ef3b304bef7140cd
Jeanne Lima <jeannelima@MacBook-Pro-de-Jeanne.local>
2022-06-18 04:10:03 -0700

    refactor(static typing): change syntax of class private attributes

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -4,31 +4,34 @@
 // Toda negociacao tem seu volume calculado multiplicando-se a quantidade negociada no dia pelo valor negociado
 
 export class Negociacao {
-  #data; // atributo de classe privado com Javascript
-  #quantidade;
-  #valor;
+  // #data; // atributo de classe privado com Javascript
+  // #quantidade;
+  // #valor;
+  private _data;
+  private _quantidade;
+  private _valor;
 
   constructor(data, quantidade, valor) {
-    this.#data = data;
-    this.#quantidade = quantidade;
-    this.#valor = valor;
+    this._data = data;
+    this._quantidade = quantidade;
+    this._valor = valor;
   }
 
   // Implementacao dos getters 
   // Estes sao como metodos que conseguem acessar os atributos privados da classe e que permitem somente leitura dos atributos da mesma, dando acesso como se fossem propriedades de classe
   get data() {
-    return this.#data;
+    return this._data;
   }
 
   get quantidade() {
-    return this.#quantidade;
+    return this._quantidade;
   }
 
   get valor() {
-    return this.#valor;
+    return this._valor;
   }
 
   get volume() {
-    return this.#quantidade * this.#valor;
+    return this._quantidade * this._valor;
   }
 }
