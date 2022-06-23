7cc24eb855f6344fa927e679cab1871488a753ca
Jeanne Lima <jeannelima@MacBook-Pro-de-Jeanne.local>
2022-06-23 07:35:36 -0700

    refactor(controller): centralize view updates in a single method in the controller

--- dist/js/controllers/negociacao-controller.js
+++ dist/js/controllers/negociacao-controller.js
@@ -13,25 +13,28 @@ export class NegociacaoController {
         this._inputValor = document.querySelector("#valor");
         this._negociacoesView.update(this._negociacoes);
     }
-    adiciona() {
-        const negociacao = this.criaNegociacao();
-        // negociacao.data.setDate(12); // Esse código altera o valor da propriedade data, pois o readonly apenas trata a permissão leitura quando se tenta fazer reatribuição, ou seja, só não deixaria que a modificação fosse feita se usado o =, o mesmo acontece com getters e para resolver somente aplicando os conceitos da programação defensiva
-        this._negociacoes.adiciona(negociacao);
+    _atualizaView() {
         this._negociacoesView.update(this._negociacoes);
         this._mensagemView.update("Negociação adicionada com sucesso!");
-        this.limparFormulario();
     }
-    criaNegociacao() {
+    _criaNegociacao() {
         const exp = /-/g; // Expressão regular para encontrar todos os hífens
         const data = new Date(this._inputData.value.replace(exp, ",")); // Converte todos os hifens em vírgula para criar um objeto Date a partir de string com ano, mês e dia separados por vírgula
         const quantidade = parseInt(this._inputQuantidade.value);
         const valor = parseFloat(this._inputValor.value);
         return new Negociacao(data, quantidade, valor);
     }
-    limparFormulario() {
+    _limparFormulario() {
         this._inputData.value = "";
         this._inputQuantidade.value = "";
         this._inputValor.value = "";
         this._inputData.focus();
+    }
+    adiciona() {
+        const negociacao = this._criaNegociacao();
+        // negociacao.data.setDate(12); // Esse código altera o valor da propriedade data, pois o readonly apenas trata a permissão leitura quando se tenta fazer reatribuição, ou seja, só não deixaria que a modificação fosse feita se usado o =, o mesmo acontece com getters e para resolver somente aplicando os conceitos da programação defensiva
+        this._negociacoes.adiciona(negociacao);
+        this._limparFormulario();
+        this._atualizaView();
     }
 }
