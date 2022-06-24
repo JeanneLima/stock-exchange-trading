716e05624f820f041db8fee65590a4c85af8aa62
Jeanne Lima <jeannelima@MacBook-Pro-de-Jeanne.local>
2022-06-24 08:14:59 -0700

    refactor(view): protect against inserting malicious scripts in templates

--- dist/js/views/view.js
+++ dist/js/views/view.js
@@ -1,13 +1,20 @@
 // Torna em classe abstrata (ou seja, não pode ser instanciada, só utilizada por meio de herança)
 // E utiliza Generics para deixar os parâmetros dos métodos com tipos a serem sobrescritos na herança 
 export class View {
-    constructor(seletor) {
+    constructor(seletor, _escapar) {
+        this._escapar = false;
         this._elemento = document.querySelector(seletor);
+        if (_escapar) {
+            this._escapar = _escapar;
+        }
     }
     // Renderiza o template no elemento capturado da DOM 
     // Método que tem uma implementação default a ser sobrescita na herança
     update(model) {
-        const template = this.template(model);
+        let template = this.template(model);
+        if (this._escapar) {
+            template = template.replace(/<script>[\s\S]*?<\/script>/, "");
+        }
         this._elemento.innerHTML = template;
     }
 }
